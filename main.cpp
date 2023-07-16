af0902a1b1c23c63ff45270e0fd4308308c65d81
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2023-07-16 23:53:49 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,39 +1,37 @@
-#include <iostream>
+#include <iostream> //chat bot
 
 using namespace std;
 
 int main(){
-    // Программа выставления оценок 
-    int score;
-        cout<< "Введите свой балл - ";
-        cin>>score;
-            if(score>=95 && score<100){
-               cout <<"A+ "<<endl;}
-                else if (score>=90 && score<95){
-                cout<<"A"<<endl;
+    string text;
+    cout<<"Hello sir!"<<endl;
+    cin>>text;
+    if(text=="hi" && "hello")
+    {
+        cout<<"My name is Jarvis. How can I help you? "<<endl;
+        cin>>text;
+        if(text=="I have to learn math, Where can I do that?" &&"i have to learn math, where can i do that?" )
+        {
+            cout<<" Oh, There is a centre called Mkh_Math centre "<<endl;
+            cin>>text;
+                if(text=="Where is that?")
+            {
+                    cout<<"It is on Termez street 46"<<endl;
+                    cin>>text;
+                
+                if(text=="I need an administration number to contact")
+                {
+                    cout<<"+998(66)239-09-99 = call them and if tell them that I send you, they will make a discount "<<endl;
+                    cin>>text;
+               
+                if(text=="Thank you ")
+                {
+                    cout<<"You are welcome!";
                 }
-                else if (score>=85 && score<90){
-                cout<<"B+"<<endl;
+                        
                 }
-                else if (score>=80 && score<85){
-                cout<<"B"<<endl;
-                }
-                else if (score>=75 && score<80){
-                cout<<"C+"<<endl;
-                }
-                else if (score>=70 && score<75){
-                cout<<"C"<<endl;
-                }
-                else if (score>=60 && score<70){
-                cout<<"D"<<endl;
-                }
-            else{cout<<"Failed, Retake please"<<endl;}
-        
-            
-    
-    
-    
-    
-    
+            }
+        }
+    }
     return 0;
 }
