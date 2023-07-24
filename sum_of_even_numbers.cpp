23be303e12a969904cdc0e40179d6abe31b837f8
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2023-07-24 17:59:48 +0500

    Update and rename sum of even numbers to sum_of_even_numbers.cpp

--- sum_of_even_numbers.cpp
+++ sum_of_even_numbers.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main()
+void calculate_with_while()
 {
     int num = 0;
     int total = 0;
@@ -13,6 +13,18 @@ int main()
         total += num;
     }
     cout << "The sum of even numbers is " << total << endl;
+}
+
+void calculate_with_for(){
+    int total = 0;
+    for(int i = 0; i <= 100; i += 2){
+        total+=i;
+    }
     
+    cout<<total<<endl;
+}
+
+int main() {
+    calculate_with_for();
     return 0;
 }
