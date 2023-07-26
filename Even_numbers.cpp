d17770fb728ff321ef01db0b725a817ed18c5138
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2023-07-26 23:54:18 +0500

    Update Even_numbers.cpp

--- Even_numbers.cpp
+++ Even_numbers.cpp
@@ -1,13 +1,33 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+void calculate_with_for (){
     int num;
     cout<<"Enter the number you want: "<<endl;
     cin>>num;
     for(int i=2; i<=num ;i+=2 ){
         cout<<" "<<i;
     }
+}
+
+
+
+
+void calculate_with_for_if()
+{
+    int N;
+    cout << "Enter integer : ";
+    cin >> N;
+ 
+    for (int i = 1; i <= N; i++)
+    {
+        if (i % 2 == 0)
+        cout << i << endl;
+    }
+}
+
+int main() {
+    calculate_with_for();
     return 0;
 }
