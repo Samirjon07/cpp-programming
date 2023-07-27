bf94f6e0f5834440bf58d41ec98a47534e0aa5e2
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2023-07-27 16:18:42 +0500

    Update 6_operators_with_switch.cpp

--- 6_operators_with_switch.cpp
+++ 6_operators_with_switch.cpp
@@ -1,3 +1,13 @@
+/*Напишите программу, которая запрашивает у пользователя два целых числа a и b. Затем предложите пользователю выбрать одну из следующих операций:
+а) Найти наибольший общий делитель (НОД) чисел a и b.
+б) Найти наименьшее общее кратное (НОК) чисел a и b.
+в) Возвести число a в степень b.
+г) Посчитать остаток от деления числа a на b.
+д) Посчитать целую часть от деления числа a на b.
+е) Посчитать квадратный корень числа a.
+При выборе каждой операции использовать оператор switch и вывести результат на экран.*/
+
+
 #include<iostream>
 #include<cmath>
 
@@ -5,26 +15,33 @@ using namespace std;
 
 int main(){
     int a,b,c,v;
-    cin>>a>>b>>v;
+    cout<<"Enter first number: "<<endl; 
+    cin>>a;
+    cout<<"Enter second number: "<<endl; 
+    cin>>b;
+    cout<<"Enter operator: "<<endl<<"1- а в b степени"<<endl<<"2-остаток от деления a на b"<<endl<<"3- целая часть от деления числа a на b"<<endl<<"4-квадратный корень числа a"<<endl<<"5-НОД(a,b)"<<endl; 
+    cin>>v;
     switch(v){
+        
+        
         case 1:
         c=pow(a,b);
-        cout<<c<<endl;
+        cout<<"а в b степени равно: "<<c<<endl;
         break;
         
         case 2:
         c=a%b;
-        cout<<c<<endl;
+        cout<<"остаток от деления a на b равно: "<<c<<endl;
         break;
         
         case 3:
         c=a/b;
-        cout<<c<<endl;
+        cout<<"целая часть от деления числа a на b равно: "<<c<<endl;
         break;
         
         case 4:
         c=sqrt(a);
-        cout<<c<<endl;
+        cout<<"квадратный корень числа a равно: "<<c<<endl;
         break;
         
         case 5:
@@ -33,8 +50,7 @@ int main(){
         a = b;
         b = c;
         }
-        cout<<a<<endl;
-        
+        cout<<"НОД(a,b) равно : "<<a<<endl;
         break;
         
         default:
